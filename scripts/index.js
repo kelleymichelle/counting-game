3884fed77a095ac1c6c054bc3059ca186848c288
Kelley Chaplain <kmchaplain@gmail.com>
2020-01-08 16:49:23 -0600

    added images

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-console.log('wired up')
+const pictureContainer = document.getElementById('picture-container')
+
+document.addEventListener("DOMContentLoaded", function() {
+  // loadTwentyBeetles()
+});
+
+// function loadTwentyBeetles() {
+//   pictureContainer.innerHTML = `<img src="images/beetles.jpg" alt="20 beetles to count">`
+// }
